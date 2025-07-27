2a54f51fb148bcdf7bc2a43df4237c020d22e17d
Dhiveej <dhiveejatish@gmail.com>
2025-07-27 20:55:26 +0530

    Delete prac1.py

--- prac1.py
+++
@@ -1,8 +0,0 @@
-a=input("ENTER A STRING:")
-for i in "0123456789":
-    count=0
-    for ch in a:
-        if ch==i:
-            count +=1
-    if count >0:
-        print(f'the frequency of {i} is {count}')
